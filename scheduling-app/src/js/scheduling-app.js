dad85fb5768edd0f26ad12c9677eb1ae18b7f65b
wonksknowsuchin <47732081+wonksknowsuchin@users.noreply.github.com>
2023-07-07 11:11:22 -0700

    Update scheduling-app.js

--- scheduling-app/src/js/scheduling-app.js
+++ scheduling-app/src/js/scheduling-app.js
@@ -67,6 +67,8 @@ function slotHTML(id, type, start, end) {
              "<h5 class='card-title'>" + type + '</h5>' +
              "<p class='card-text'>Start: " + prettyStart + '</p>' +
              "<p class='card-text'>End: " + prettyEnd + '</p>' +
+             "<a href='javascript:void(0);' class='card-link' onclick='appointmentCreate(\"" +
+               slotReference + "\", \"Patient/12508016\");'>Book</a>" +
            '</div>' +
          '</div>';
 }
